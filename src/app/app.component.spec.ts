e7aeee30587efc726fdc2f81684b79c27f2118b3
Kaique Alves <kaiqueexp@gmail.com>
2024-09-13 23:29:58 -0300

    alteracao nome do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'children-allowance' title`, () => {
+  it(`should have the 'taskids' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('children-allowance');
+    expect(app.title).toEqual('taskids');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, children-allowance');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, taskids');
   });
 });
